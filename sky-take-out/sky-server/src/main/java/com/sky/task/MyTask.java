9936f8d98f91421cbedd927a6ebbb0e826fbaec8
comeback12345 <1827896640@qq.com>
2024-09-07 15:20:52 +0800

    订单状态定时处理

---
+++ sky-take-out/sky-server/src/main/java/com/sky/task/MyTask.java
@@ -0,0 +1,20 @@
+package com.sky.task;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+
+/**
+ * 自定义定时任务类
+ */
+@Slf4j
+public class MyTask {
+
+    @Scheduled(cron = "* * * * * ? *")
+    public void execute(){
+        log.info("定时任务开始执行：{}",new Date());
+    }
+}
