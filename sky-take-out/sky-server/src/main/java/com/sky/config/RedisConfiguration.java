3ac424a98da7ffaf2826b19d8cee585f867e14c7
comeback12345 <1827896640@qq.com>
2025-04-17 18:44:48 +0800

    更新

--- sky-take-out/sky-server/src/main/java/com/sky/config/RedisConfiguration.java
+++ sky-take-out/sky-server/src/main/java/com/sky/config/RedisConfiguration.java
@@ -17,6 +17,7 @@ public class RedisConfiguration {
         log.info("开始创建redis模板对象...");
         //StringRedisTemplate stringRedisTemplate = new StringRedisTemplate();
         //redis客户端显示正常
+        /*
         RedisTemplate redisTemplate = new RedisTemplate();
 
         //设置redis的连接工厂对象
@@ -27,6 +28,28 @@ public class RedisConfiguration {
         //stringRedisTemplate.setKeySerializer(new StringRedisSerializer());
         //return stringRedisTemplate;
         redisTemplate.setKeySerializer(new StringRedisSerializer());
+        return redisTemplate;
+        */
+
+        // 创建 RedisTemplate 对象
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+
+        // 设置连接工厂
+        redisTemplate.setConnectionFactory(connectionFactory);
+
+        // 设置 Key 的序列化器
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+
+        // 设置 Value 的序列化器，防止中文乱码
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+
+        // 如果使用 Hash 类型，还需要单独设置 HashKey 和 HashValue 的序列化器
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new StringRedisSerializer());
+
+        // 初始化配置
+        redisTemplate.afterPropertiesSet();
+
         return redisTemplate;
     }
 }
